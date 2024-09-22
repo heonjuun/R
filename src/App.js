7bd621fc7219399115c3093f7566197376971a49
heonjunkim <s01077314486@gmail.com>
2024-09-22 21:32:16 +0900

    게시판이미지업로드추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './App.css';
 import Layout from './components/layout/layout';
 import MainBoard from './components/layout/main/mainboard';
-
 import RegionBoardList from './components/layout/main/region_board_list';
 import FestivalBoardList from './components/layout/main/festival_board_list';
 import WritingPage from './components/layout/main/WritingPage';
